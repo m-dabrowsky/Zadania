83e8bbe79d36b60cc1ad8f4ae620d22be8527c52
Michal Dabrowski <michal_dabrowski@onet.eu>
2021-03-08 12:34:59 +0100

    Program wyświetlajacy stała PI = 3.14

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
+/*
+ Napisz program który wyświetli na ekranie komputera wartość predefiniowanej stałej Pi = 3.14
+Należy przyją format wyświetlania tej stałej z dokładnościa do pięciu miejsc po przecinku
+ */
+
+
 namespace Zadanie2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }bhgfhfgffdg   
+            Console.WriteLine("PI= {0:#.#####}", Math.PI);
+        }  
     }
 }
-\\]\
