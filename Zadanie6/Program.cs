95af2f9625ec3fa547749bcc3d0db85d8a8752a4
Michal Dabrowski <michal_dabrowski@onet.eu>
2021-03-08 13:56:38 +0100

    Program który za pomocą for sumuje liczby parzyste od 1-100 Program który za pomocą do--while sumuje liczby parzyste od 1-100

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ Program który za pomocą for sumuje liczby parzyste od 1-100
+ Program który za pomocą do--while sumuej liczby parzyste od 1-100
+ */
+
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int suma = 0;
+
+            // dla for
+            for (i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma = suma + i;
+                 
+                }
+            }
+            Console.WriteLine(suma);
+
+
+            // dla do.. while
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    suma = suma + i;
+                    i++;
+                }
+            }
+            while (i <= 100);
+            Console.WriteLine(suma);
+        }
+    }
+}
